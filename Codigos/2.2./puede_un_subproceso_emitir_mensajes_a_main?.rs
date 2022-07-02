fb95437cf1627e0af301a55f6a1e5733d5735900
Sansanto2000 <64858429+Sansanto2000@users.noreply.github.com>
2022-07-02 20:48:12 -0300

    Create puede_un_subproceso_emitir_mensajes_a_main?.rs

---
+++ Codigos/2.2./puede_un_subproceso_emitir_mensajes_a_main?.rs
@@ -0,0 +1,16 @@
+use std::sync::mpsc;
+use std::thread;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+
+    let handle = thread::spawn(move || {
+        let received = rx.recv().unwrap();
+        println!("Hilo> Hola, acabo de recibir el mensaje de Main [mensaje = {}]", received);
+    });
+    let val = String::from("hi");
+    tx.send(val).unwrap();
+    println!("Main> Hola, acabo de enviar un mensaje");
+    handle.join().unwrap();
+    println!("Main> Acabo de ejecutar un join sobre Hilo y ahora voy a finalizar mi ejecucion");
+}
