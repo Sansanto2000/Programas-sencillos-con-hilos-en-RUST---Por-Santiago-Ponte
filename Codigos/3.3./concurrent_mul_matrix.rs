87c9c06a17dc10a38b91711c3c0c76855534a285
Santiago Ponte <64858429+Sansanto2000@users.noreply.github.com>
2022-07-24 21:07:20 -0300

    Update concurrent_mul_matrix.rs

--- Codigos/3.3./concurrent_mul_matrix.rs
+++ Codigos/3.3./concurrent_mul_matrix.rs
@@ -2,6 +2,21 @@ use std::sync::Mutex;
 use std::thread;
 use std::sync::Arc;
 
+pub fn mul_matrix(n: usize, m: usize, a: &[f64], b: &[f64]) -> Vec<f64> {
+    let mut sum:f64;
+    let mut c:Vec<f64> = Vec::with_capacity(n*m);
+    for i in 0..n {
+        for j in 0..m {
+            sum = 0.0;
+            for k in 0..m {
+                sum += &a[i*m+k] * &b[j*m+k];   //Santiago> Si 'b' no estubiese transpuesta, esta linea seria: 'sum += &a[i*m+k] * &b[k*m+j];'
+            }
+            c.push(sum);
+        }
+    }
+    c
+} 
+
 pub fn concurrent_mul_matrix(threads:usize ,n: usize, m: usize, a: Vec<f64>, b: Vec<f64>) -> Vec<f64> {
     let mut handles = Vec::new();
     let c:Vec<f64> = vec![0.0; n*m];
